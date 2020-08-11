e0e75a8ed4f1b7da08e1e6e30a0dc9afeb7851a9
Fernando Kobayashi <fmkoba@gmail.com>
2020-08-11 19:33:48 -0300

    setting up Gatsby and Netlify Identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const initNetlifyIdentity = () => {
+  const script = document.createElement('script');
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+  script.async = true;
+  document.body.appendChild(script);
+}
+
+const openNetlifyModal = () => {
+  if (! window.netlifyIdentity) {
+    console.log('netlify script not loaded');
+    return;
+  }
+  window.netlifyIdentity.open();
+}
+
+const IndexPage = () => {
+  useEffect(() => {
+    initNetlifyIdentity();
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <button onClick={ openNetlifyModal }>Login</button>
+      <p>Welcome to your new Gatsby site.</p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Layout>
+  );
+}
 
 export default IndexPage
